c38afe53e3d308f292a691ab5e59952c283c7160
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-07-09 22:37:49 +0200

    Refactor book creation logic and improve form handling

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // DOM selectors
-const tableBody = document.querySelector('#table_body');
-const addBookBtn = document.querySelector('#add_book_btn');
+const tableBody = document.querySelector('#table__body');
+// modal
+const dialog = document.querySelector('#dialog');
+const addBookBtn = document.querySelector('#add-book__btn');
+const cancelBtn = document.querySelector('#cancel__btn');
+
+// form
+const form = document.querySelector('#form');
+const inputTitle = document.querySelector('#input__title');
+const inputAuthor = document.querySelector('#input__author');
+const inputPages = document.querySelector('#input__pages');
+const inputStatus = document.querySelector('#input__status');
 
 // Code logic
 const myLibrary = [];
@@ -23,50 +33,69 @@ Book.prototype.generateID = function () {
 	// It creates a 128-bit identifier (chance of collision is extremely low: it uses the OS’s underlying cryptographic random number generator). In this case, there is no need to check if the id has already been generated in previous runtimes.
 };
 
-// take params, create a book then store it in the array
-function addBookToLibrary(title, author, pages, status) {
-	const newBook = new Book(title, author, pages, status);
+// take params, create a book, return it
+function createBook(title, author, pages, status) {
+	return new Book(title, author, pages, status);
+}
+
+// display form
+addBookBtn.addEventListener('click', () => {
+	dialog.showModal();
+});
+
+// close form
+cancelBtn.addEventListener('click', () => {
+	dialog.close();
+});
+
+// form to submit new book
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const newBook = createBook(
+		inputTitle.value,
+		inputAuthor.value,
+		inputPages.value,
+		inputStatus.value
+	);
+
+	// add book to myLibrary
 	myLibrary.push(newBook);
+	// display book
+	displayNewBook(newBook);
+	// clear the form values
+	form.reset();
+	dialog.close();
+});
+
+function displayNewBook(book) {
+	console.log(myLibrary);
+
+	// row
+	const row = document.createElement('tr');
+	// data fields
+	const title = document.createElement('td');
+	const author = document.createElement('td');
+	const pages = document.createElement('td');
+	const status = document.createElement('td');
+
+	// styles
+	title.classList.add('title');
+	pages.classList.add('pages');
+	status.classList.add('status');
+
+	// book info to display
+	title.textContent = book.title;
+	author.textContent = book.author;
+	pages.textContent = book.pages;
+	status.textContent = book.status;
+
+	// append data to row
+	row.appendChild(title);
+	row.appendChild(author);
+	row.appendChild(pages);
+	row.appendChild(status);
+
+	// append row to table
+	tableBody.appendChild(row);
 }
-
-// add some data manually to check if displayBooks works as intended
-addBookToLibrary('Book 1', 'Author 1', 322, true);
-addBookToLibrary('Book 2', 'Author 2', 1, true);
-addBookToLibrary('Book 3', 'Author 3', 99999, false);
-
-console.table(myLibrary);
-
-function displayBooks(array) {
-	// for each book create
-	array.forEach((book) => {
-		// row
-		const row = document.createElement('tr');
-		// data fields
-		const title = document.createElement('td');
-		const author = document.createElement('td');
-		const pages = document.createElement('td');
-		const status = document.createElement('td');
-
-		// styles
-		title.classList.add('title');
-		pages.classList.add('pages');
-		status.classList.add('status');
-
-		// book info to display
-		title.textContent = book.title;
-		author.textContent = book.author;
-		pages.textContent = book.pages;
-		status.textContent = book.status;
-
-		// append data to row
-		row.appendChild(title);
-		row.appendChild(author);
-		row.appendChild(pages);
-		row.appendChild(status);
-
-		// append row to table
-		tableBody.appendChild(row);
-	});
-}
-
-displayBooks(myLibrary);
